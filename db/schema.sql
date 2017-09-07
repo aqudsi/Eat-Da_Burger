92320a06444730ad916fc47e712a04d5faaeafb4
aqudsi <ahmerqudsi@gmail.com>
2017-09-07 19:09:09 -0400

    w/e

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
-use gz1mhyg7ri60r9ta;
+use db_name;
 CREATE TABLE burgers(
 id INT(11) auto_increment not null,
 burger_name VARCHAR(100),
 devoured BOOLEAN,
-date TIMESTAMP,
+date TIMESTAMP NOT NULL,
 PRIMARY KEY(id)
 );
-
 describe burgers;
