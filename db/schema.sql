8ce24dcb9f70b81115d2088fee452d22423bdab7
aqudsi <ahmerqudsi@gmail.com>
2017-09-07 18:53:35 -0400

    noob

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
-CREATE DATABASE  sequelize_library;
-USE sequelize_library;
+use gz1mhyg7ri60r9ta;
+CREATE TABLE burgers(
+id INT(11) auto_increment not null,
+burger_name VARCHAR(100),
+devoured BOOLEAN,
+date TIMESTAMP,
+PRIMARY KEY(id)
+);
+
+describe burgers;
